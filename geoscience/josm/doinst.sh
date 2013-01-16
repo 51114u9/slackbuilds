0e88f3e0b353949369e68a710355c3483d86efb5
Marco Antonio Frias Butrón <marcoantoniofrias@gmail.com>
2013-01-15 20:50:14 -0400

    josm: Upgraded

--- geoscience/josm/doinst.sh
+++ geoscience/josm/doinst.sh
@@ -3,3 +3,13 @@
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications
 fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime > /dev/null 2>&1
+fi
+
+if [ -x /usr/bin/gtk-update-icon-cache ]; then
+  if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+    /usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor > /dev/null 2>&1
+  fi
+fi
--- geoscience/josm/josm.sh
+++ geoscience/josm/josm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2012 Marco Antonio Frias B., Cochabamba, BO
+# Copyright 2012-2013 Marco Antonio Frias B., Cochabamba, BO
 # All rights reserved.
 #
 # Redistribution and use of this script, with or without modification, is
@@ -35,7 +35,7 @@ fi
 RUNJAVA="$JRE_HOME"/bin/java
 
 # Set full path to app home directory
-APP_HOME=/usr/share/@APP@
+APP_HOME=/usr/share/josm
 
 # Execute
-$RUNJAVA -jar $APP_HOME/@JARNAME@.jar "$@"
+$RUNJAVA -Xms64M -Xmx1024M -XX:+UseParallelGC -XX:+UseAdaptiveSizePolicy -jar $APP_HOME/@JARNAME@.jar "$@"

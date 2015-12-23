4badde898e68469006bb58c9685dc38879f12065
51114u9 <marcoantoniofrias@gmail.com>
2015-12-22 21:24:44 -0400

    xfce4-whiskermenu-plugin: removed

--- xfce/xfce4-whiskermenu-plugin/doinst.sh
+++
@@ -1,6 +0,0 @@
-
-if [ -x /usr/bin/gtk-update-icon-cache ]; then
-  if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
-    /usr/bin/gtk-update-icon-cache -f -q usr/share/icons/hicolor > /dev/null 2>&1
-  fi
-fi
